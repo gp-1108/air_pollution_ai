28d5655a6341ae4336eb39c427e98c7aa99f9e64
gp-1108 <pietrogirotto00@gmail.com>
2023-05-25 16:44:32 +0200

    filtered data by cause_id for different trainings purposes

--- training_dataset_generator.py
+++ training_dataset_generator.py
@@ -16,10 +16,31 @@ df_air = df_air.dropna()
 # Drop the rows with fips that are not in the mortality dataset
 df_air = df_air[df_air['fips'].isin(df_mortality['fips'])]
 
+"""
+cause_id columns meaning:
+  "508","Chronic respiratory diseases"
+  "509","Chronic obstructive pulmonary disease"
+  "510","Pneumoconiosis"
+  "511","Silicosis"
+  "512","Asbestosis"
+  "513","Coal workers pneumoconiosis"
+  "514","Other pneumoconiosis"
+  "515","Asthma"
+  "516","Interstitial lung disease and pulmonary sarcoidosis"
+  "520","Other chronic respiratory diseases"
+"""
 
 final_df = df_air.merge(df_mortality, on=['fips', 'year'])
 
-print(final_df.shape)
+# For each cause_id create a new csv and save it
+for cause_id in range(508, 521):
+  df = final_df.copy()
+  df = df.rename(columns={'rate': 'mortality_rate'})
+  df = df[df['cause_id'] == cause_id]
+  df.to_csv(f'training_data/cause_id_{cause_id}.csv', index=False)
+  print(f"Saved cause_id_{cause_id}.csv with shape: {df.shape}")
+
 
 # Save it to csv
-final_df.to_csv('training_data.csv', index=False)
+final_df.to_csv('training_data/all_data.csv', index=False)
+
